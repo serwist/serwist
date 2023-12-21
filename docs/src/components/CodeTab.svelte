98afb37d585ee5877e2c8e0fe5ed9ca6e09a82cd
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-12-21 14:04:41 +0700

    chore(docs): fix some issues listed by PageSpeed Insights

--- docs/src/components/CodeTab.svelte
+++ docs/src/components/CodeTab.svelte
@@ -25,7 +25,7 @@
   });
 </script>
 
-<div class="w-full rounded-xl bg-neutral-50 dark:bg-neutral-950 flex flex-col overflow-hidden border-[0.5px] border-gray-300 dark:border-gray-800">
+<div class="w-full rounded-xl bg-white dark:bg-neutral-950 flex flex-col overflow-hidden border-[0.5px] border-gray-300 dark:border-gray-800">
   <div class="w-full bg-white dark:bg-black relative flex overflow-auto">
     {#each codeKeys as tab}
       {@const isActive = tab === currentTab}
@@ -33,14 +33,14 @@
         id={`${idPrefix}-${tab}-button`}
         class={clsx(
           "py-2 px-4 border-gray-300 dark:border-gray-800 relative border-r-[0.5px]",
-          isActive ? "bg-neutral-50 text-black dark:bg-neutral-950 dark:text-white" : "text-gray-600 dark:text-gray-400"
+          isActive ? "bg-white text-black dark:bg-neutral-950 dark:text-white" : "text-gray-600 dark:text-gray-400"
         )}
         on:click={() => (currentTab = tab)}
         aria-controls={`${idPrefix}-${tab}-code`}
       >
         {tab}
         {#if isActive}
-          <span class="w-full h-[1px] z-[2] bg-neutral-50 dark:bg-neutral-950 absolute bottom-0 left-0 pointer-events-none" aria-hidden="true" />
+          <span class="w-full h-[1px] z-[2] bg-white dark:bg-neutral-950 absolute bottom-0 left-0 pointer-events-none" aria-hidden="true" />
         {/if}
       </button>
     {/each}
@@ -51,14 +51,18 @@
       {@const isActive = tab === currentTab}
       <div
         id={`${idPrefix}-${tab}-code`}
-        class={clsx("whitespace-normal overflow-auto [&>*]:!bg-transparent", !isActive && "hidden")}
+        class={clsx("whitespace-normal overflow-auto", !isActive && "hidden")}
         aria-labelledby={`${idPrefix}-${tab}-button`}
       >
-        <!-- Only use trusted code! -->
-        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-        {@html code.dark}
-        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-        {@html code.light}
+        <span class="code-tab-dark [&>*]:!bg-transparent">
+          <!-- Only use trusted code! -->
+          <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+          {@html code.dark}
+        </span>
+        <span class="code-tab-light [&>*]:!bg-transparent">
+          <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+          {@html code.light}
+        </span>
       </div>
     {/each}
   </div>
