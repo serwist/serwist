4ff93c6a7c25a4e3696ae7ece8d5c308e8715cf2
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2024-02-15 00:01:08 +0700

    chore(docs): added static revisions

--- docs/src/service-worker.ts
+++ docs/src/service-worker.ts
@@ -3,7 +3,7 @@
 /// <reference lib="webworker" />
 /// <reference types="@sveltejs/kit" />
 import type { SerwistGlobalConfig } from "@serwist/core";
-import { defaultCache, defaultIgnoreUrlParameters, getPrecacheManifest } from "@serwist/svelte/worker";
+import { type StaticRevisions, staticAssets, defaultCache, defaultIgnoreUrlParameters, getPrecacheManifest } from "@serwist/svelte/worker";
 import { installSerwist } from "@serwist/sw";
 
 declare global {
@@ -14,8 +14,17 @@ declare const self: ServiceWorkerGlobalScope;
 
 self.__WB_CONCURRENT_PRECACHING = 10;
 
+// IMPORTANT NOTE: BUMP THIS UP SHOULD YOU UPDATE
+// THE STATIC FOLDER.
+const staticRevision = "serwist-docs-static-v1";
+
 installSerwist({
-  precacheEntries: getPrecacheManifest(),
+  precacheEntries: getPrecacheManifest({
+    staticRevisions: staticAssets.reduce((prev, cur) => {
+      prev[cur] = staticRevision;
+      return prev;
+    }, {} as StaticRevisions),
+  }),
   precacheOptions: {
     ignoreURLParametersMatching: defaultIgnoreUrlParameters,
   },
