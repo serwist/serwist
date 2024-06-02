58b0e9b61274b6cbfc62ee7b451b781f30bd3fea
Ngô Đức Anh <75556609+DuCanhGH@users.noreply.github.com>
2024-06-02 16:21:58 +0700

    fix(examples): fixed next-web-push not working
    Fixes #149

--- examples/next-web-push/app/sw.ts
+++ examples/next-web-push/app/sw.ts
@@ -31,4 +31,32 @@ const serwist = new Serwist({
   },
 });
 
+self.addEventListener("push", (event) => {
+  const data = JSON.parse(event.data?.text() ?? '{ title: "" }');
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.message,
+      icon: "/icons/android-chrome-192x192.png",
+    }),
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return self.clients.openWindow("/");
+    }),
+  );
+});
+
 serwist.addEventListeners();
