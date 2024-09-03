eecfe3664991988d52586579393a995074e22a65
DuCanhGH <ngoducanh2912@gmail.com>
2024-09-04 00:07:49 +0700

    feat: better Tabs

--- docs/src/components/Tabs.svelte
+++ docs/src/components/Tabs.svelte
@@ -1,30 +1,40 @@
-<script lang="ts" generics="T extends [tab: string, id: string, renderer: Snippet]">
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  import type { Snippet } from "svelte";
+<script lang="ts" module>
+  export interface TabsState {
+    id: string;
+    current: string;
+  }
+</script>
+
+<script lang="ts" generics="T extends string">
+  import { setContext, type Snippet } from "svelte";
 
   import { clsx } from "$lib/clsx";
 
-  type TKeys = T[0];
-
-  interface CodeTabProps {
+  interface TabsProps {
     id: string;
-    codes: T[];
-    defaultTab: TKeys;
+    tabs: [name: string, id: T][];
+    children: Snippet;
   }
 
-  const { id, codes, defaultTab }: CodeTabProps = $props();
-  let currentTab = $state(defaultTab);
+  const { id, tabs, children }: TabsProps = $props();
 
-  $effect(() => {
-    currentTab = defaultTab;
+  let currentTab = $state(tabs[0][1]);
+
+  $effect(() => void (currentTab = tabs[0][1]));
+
+  setContext<TabsState>("tabsState", {
+    id,
+    get current() {
+      return currentTab;
+    },
   });
 </script>
 
 <div class="dark:bg-neutral-925 my-3 flex w-full flex-col rounded-xl border border-neutral-300 bg-white shadow-md dark:border-neutral-800">
   <div class="relative w-full rounded-t-xl bg-white dark:bg-black">
     <div role="tablist" aria-orientation="horizontal" class="flex w-full overflow-auto rounded-t-xl">
-      {#each codes as [tab, tabId]}
-        {@const isActive = tab === currentTab}
+      {#each tabs as [tab, tabId]}
+        {@const isActive = tabId === currentTab}
         <button
           id={`${id}-${tabId}-button`}
           role="tab"
@@ -34,7 +44,7 @@
             "relative min-w-max border-r border-neutral-300 px-4 py-2 dark:border-neutral-800",
             isActive ? "dark:bg-neutral-925 bg-white text-black dark:text-white" : "text-neutral-600 dark:text-neutral-400"
           )}
-          onclick={() => (currentTab = tab)}
+          onclick={() => (currentTab = tabId)}
         >
           {tab}
           {#if isActive}
@@ -46,13 +56,6 @@
     <div class="pointer-events-none absolute bottom-0 left-0 z-[1] h-[1px] w-full bg-neutral-300 dark:bg-neutral-800" aria-hidden="true"></div>
   </div>
   <div class="margin-0 overflow-auto p-4">
-    {#each codes as [tab, tabId, code]}
-      {@const isActive = tab === currentTab}
-      <div role="tabpanel" id={`${id}-${tabId}-code`} class="whitespace-normal" class:hidden={!isActive} aria-labelledby={`${id}-${tabId}-button`}>
-        {#if isActive}
-          {@render code()}
-        {/if}
-      </div>
-    {/each}
+    {@render children()}
   </div>
 </div>
