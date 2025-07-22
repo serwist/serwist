93cf078a4fdc3a306b0aa796fd3214d258413c7b
DuCanhGH <ngoducanh2912@gmail.com>
2025-07-23 02:14:45 +0700

    feat(turbo): added rudimentary Turbopack support
    [bump]

--- packages/turbo/src/index.schema.ts
+++ packages/turbo/src/index.schema.ts
@@ -1,1 +1,22 @@
-export {};
+import path from "node:path";
+import { basePartial, globPartial, injectPartial } from "@serwist/build/schema";
+import z from "zod";
+import { DEFAULT_GLOB_PATTERNS } from "./lib/constants.js";
+
+export const injectManifestOptions = z
+  .strictObject({
+    ...basePartial.shape,
+    ...globPartial.shape,
+    ...injectPartial.shape,
+    cwd: z.string().prefault(process.cwd()),
+    basePath: z.string(),
+    globPatterns: z.array(z.string()).prefault(DEFAULT_GLOB_PATTERNS),
+    globDirectory: z.string().optional(),
+  })
+  .transform((input) => {
+    return {
+      ...input,
+      swSrc: path.isAbsolute(input.swSrc) ? input.swSrc : path.join(input.cwd, input.swSrc),
+      globDirectory: input.globDirectory ?? input.cwd,
+    };
+  });
---
+++ packages/turbo/src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_GLOB_PATTERNS = [
+  ".next/static/**/*.{js,css,html,ico,apng,png,avif,jpg,jpeg,jfif,pjpeg,pjp,gif,svg,webp,json,webmanifest}",
+  "public/**/*",
+];
